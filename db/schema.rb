fa875022010f0b106dcbb03551bd5854ad6b7a76
Robert Tate <btate712@gmail.com>
2019-04-14 20:37:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -29,8 +27,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.string "size"
     t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -42,8 +38,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "opening_date"
     t.datetime "closing_date"
     t.string "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
